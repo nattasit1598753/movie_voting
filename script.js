306230b53abbe0af9d97d463a3d5b1359438528f
nattasit1598753 <123621081+nattasit1598753@users.noreply.github.com>
2026-02-08 18:24:27 +0700

    Update movie list and Firebase configuration

--- script.js
+++ script.js
@@ -11,11 +11,16 @@ const firebaseConfig = {
 };
 
 const movies = [
-  { id: "1", title: "Interstellar", poster: "https://upload.wikimedia.org/wikipedia/th/b/bc/Interstellar_film_poster.jpg" },
-  { id: "2", title: "Inception", poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9gk7Fn9sVAsS9o99G9NET6YvUfw.jpg" },
-  { id: "3", title: "Dune", poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/d5NXSklfzG0pGOBJ9vGvHXv62pE.jpg" },
-  { id: "4", title: "Avatar", poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jRXYjXuv8V2Y1XDeYhApyp6OUC1.jpg" },
-  { id: "5", title: "Blade Runner 2049", poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/gajva2L0rQvSfkvMvyG6oGv9R3o.jpg" }
+  { id: "1", title: "Green mile", poster: "https://m.media-amazon.com/images/I/81ryvZtPc7L._AC_UF894,1000_QL80_.jpg" },
+  { id: "2", title: "The Wonderful Story of Henry Sugar", poster: "https://resizing.flixster.com/W1q4CGmGVpsPDrxRK3-liMKCDx8=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p26972315_v_v7_aa.jpg" },
+  { id: "3", title: "Knives Out", poster: "https://m.media-amazon.com/images/S/pv-target-images/9994a930958ea8f01eab73600a3f42235af108d1740343d632ed0b5cf02701f6.jpg" },
+  { id: "4", title: "Predestination", poster: "https://m.media-amazon.com/images/I/91eAqabf6cL._AC_UF894,1000_QL80_.jpg" },
+  { id: "5", title: "Source Code", poster: "https://m.media-amazon.com/images/M/MV5BMTY0MTc3MzMzNV5BMl5BanBnXkFtZTcwNDE4MjE0NA@@._V1_.jpg" },
+  { id: "6", title: "Inception", poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg" },
+  { id: "7", title: "Gone Girl", poster: "https://f.ptcdn.info/666/053/000/owf4fcgtqpiJX6F7wwT-o.jpg" },
+  { id: "8", title: "A Beautiful Mind:", poster: "https://upload.wikimedia.org/wikipedia/th/thumb/b/b8/A_Beautiful_Mind_Poster.jpg/250px-A_Beautiful_Mind_Poster.jpg" },
+  { id: "9", title: "เอ๋อเหรอ", poster: "https://upload.wikimedia.org/wikipedia/th/thumb/9/91/Beautifulwonderfulperfect.jpg/250px-Beautifulwonderfulperfect.jpg" },
+  { id: "10", title: "มหาลัยเหมืองแร่", poster: "https://f.ptcdn.info/514/024/000/1413372625-1-o.jpg" }
 ];
 
 let selected = [];
@@ -107,6 +112,7 @@ function submitVote(votes) {
 
 
 console.log("Firebase connected:", db);
+
 
 
 
